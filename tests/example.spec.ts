c630a91942c57c08e3539aa1c6ca666d11818d1a
OlivierL <o.lam@bilendi.com>
2025-10-14 01:21:34 +0200

    update ci config, update workflow yaml and fix test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,6 +4,6 @@ test('get started link', async ({ page }) => {
   // Access to the website.
   await page.goto('https://olivierlam.fr/index2.html');
 
-  // Expects page to have a heading with Olivier LAM.
-  await expect(page.getByRole('heading', { name: 'Olivier LAM' })).toBeVisible();
+  // Expect title to containt Salut ! Je suis Olivier Lam
+  await expect(page.getByRole('heading', { name: 'Salut ! Je suis Olivier Lam' })).toBeVisible();
 });
