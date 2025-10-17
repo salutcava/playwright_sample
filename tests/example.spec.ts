65b39e8b9211084ca2e20c132fe1be9778687654
OlivierL <o.lam@bilendi.com>
2025-10-17 10:39:15 +0200

    cross workflow

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,9 +1,13 @@
 import { test, expect } from '@playwright/test';
 
-test('get started link', async ({ page }) => {
-  // Access to the website.
-  await page.goto('https://olivierlam.fr/index2.html');
+test('Simple login test', async ({ page }) => {
+  await page.goto('https://salutcava.github.io/playwright_sample_site/');
+  await expect(page.getByRole('heading', { name: 'Entrez vos identifiants pour continuer' })).toBeVisible();
 
-  // Expect title to containt Salut ! Je suis Olivier Lam
-  await expect(page.getByRole('heading', { name: 'Salut ! Je suis Olivier Lam' })).toBeVisible();
+  const loginButton = page.getByRole('button', { name: 'Se connecter' });
+  await expect(loginButton).toBeVisible();
+  await expect(loginButton).toBeEnabled();
+  await loginButton.click();
+  
+  await expect(page.getByText('Bienvenue Olivier')).toBeVisible();
 });
