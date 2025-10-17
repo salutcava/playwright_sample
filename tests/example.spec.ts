9b4407579dc70df931fc4309026886ffabc2fcc8
OlivierL <o.lam@bilendi.com>
2025-10-17 15:11:54 +0200

    update test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -4,12 +4,12 @@ test('Simple login test', async ({ page }) => {
   await page.goto('https://salutcava.github.io/playwright_sample_site/');
   await expect(page.getByText('Entrez vos identifiants pour continuer')).toBeVisible();
 
-  const loginButton = page.getByRole('button', { name: 'Se connecter' });
+  const loginButton = page.getByRole('button', { name: 'Se connecter', exact: true });
   await expect(loginButton).toBeVisible();
   await expect(loginButton).toBeEnabled();
   await loginButton.click();
 
   await expect(page.getByText('Bienvenue Olivier')).toBeVisible();
-  
+
   await page.waitForTimeout(1000);
 });
